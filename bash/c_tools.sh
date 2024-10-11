e90937ae9a1cac85bacb0b5692239c2371066733
WAROL52 <rabetsyrolio@gmail.com>
2024-10-11 10:32:20 +0300

    mise a jour c_tools

--- bash/c_tools.sh
+++ bash/c_tools.sh
@@ -16,6 +16,22 @@ function valgrind_run(){
 	echo -e "${Normal}"
 }
 
+function helgrind_run(){
+	local valgrind_out=$OUT_PATH/out_valgrind
+	local progname=$1
+	shift
+	echo -e "${Blank}$progname  $@ ${Normal}${Normal}"
+	local t1=$(date '+%s')
+	(valgrind --tool=helgrind $progname "$@" ) 2> $valgrind_out
+	local t2=$(date '+%s')
+	timesDiff=$(($t2 - $t1))
+	echo -e "${Reverse}"
+	echo "Temps: $timesDiff ms "
+	grep "in use at exit" < $valgrind_out | sed 's/^==[0-9]*==     in use at exit/Memoire/'
+	grep "total heap usage" < $valgrind_out | sed "s/^==[0-9]*==   total heap usage/Allocation/"
+	echo -e "${Normal}"
+}
+
 function make_all(){
 	local oldpwd=$PWD
 	mkdir -p $OUT_PATH
